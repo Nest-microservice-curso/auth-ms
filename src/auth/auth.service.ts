fe9b1f322818a08c51103067e37a62cb3e3b2d11
LauchoIT <contacto@gmail.com>
2024-03-24 12:53:36 -0400

    Add Register user and login user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { LoginDto, RegisterDto } from './dto';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {
-  registerUser(registerDto) {
-    return { ...registerDto, mensaje: 'desde el microservicio, registerUser' };
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private logger = new Logger('auth.service.ts');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.debug('Database connected');
   }
-  loginUser(loginDto) {
-    return { ...loginDto, mensaje: 'desde el microservicio, loginUser' };
+
+  async registerUser(registerDto: RegisterDto) {
+    this.logger.warn('Entrando en el metodo para registrar');
+    const user = await this.user.findUnique({
+      where: {
+        email: registerDto.email,
+      },
+    });
+    this.logger.warn('Despues de la busqueda del usuario en la BD');
+
+    if (user) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: `The email ${registerDto.email} has taken`,
+      });
+    }
+    const newUser = await this.user.create({
+      data: {
+        email: registerDto.email,
+        name: registerDto.name,
+        password: bcrypt.hashSync(registerDto.password, 12),
+      },
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: __, ...data } = newUser;
+    return {
+      user: data,
+      token: 'sdfghgfdsasdfgh',
+    };
   }
-  verifyToken(tokenDto) {
-    return { ...tokenDto, mensaje: 'desde el microservicio, verifyToken' };
+  async loginUser(loginDto: LoginDto) {
+    const user = await this.user.findUnique({
+      where: {
+        email: loginDto.email,
+      },
+    });
+
+    if (!user) {
+      throw new RpcException({
+        status: HttpStatus.UNAUTHORIZED,
+        message: `Email/Password not found, please try again`,
+      });
+    }
+
+    if (!bcrypt.compareSync(loginDto.password, user.password)) {
+      throw new RpcException({
+        status: HttpStatus.UNAUTHORIZED,
+        message: `Email/Password not found, please try again`,
+      });
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: __, ...data } = user;
+    return {
+      user: data,
+      token: 'fghjkllkjhgfghjk',
+    };
+  }
+  verifyToken(tokenDto: string) {
+    return { tokenDto, mensaje: 'desde el microservicio, verifyToken' };
   }
 }
